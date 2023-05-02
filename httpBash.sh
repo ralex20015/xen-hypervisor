8277e3f6045c1d306a11dd4701e5ed529a8eaefd
Alex Robles <lozanoeduardo8777@gmail.com>
2023-05-01 23:30:30 -0500

    http server script done

--- httpBash.sh
+++ httpBash.sh
@@ -1,4 +1,18 @@
 apt-get install apache2 -y
 apt-get install php -y
 apt-get install php-cgi -y
-
+apt-get install php-gd -y
+apt-get install php-mysql -y
+apt-get install php-xsl -y
+apt-get install libapache2-mod-php -y
+apt-get install libmagic-dev -y
+apt-get install php-ldap -y
+apt-get install php-imap -y
+apt-get install php-dev -y
+apt-get install php-pear -y
+apt-get install make -y
+apt-get install php-net-socket -y
+cd /var/www/
+git clone https://github.com/ralex20015/Ifood_proyecto_web.git
+rm -r html
+mv ./Ifood_proyecto_web ./html
